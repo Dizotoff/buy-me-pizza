4231b3408920c2922ba86af89810a201148364b4
Dmytro Izotov <izotov.dima@gmail.com>
2022-05-17 22:17:30 +0300

    Add index page styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      transparent: "transparent",
+      black: "black",
+      white: "white",
+      neutral: {
+        100: "#F4F5F6",
+        300: "#DEE3E2",
+        500: "#C8CED0",
+        700: "#8E999B",
+        900: "#656E71",
+      },
+      primary: {
+        100: "#42F1DA",
+        500: "#8972DF",
+      },
+    },
   },
   plugins: [],
-}
+};
